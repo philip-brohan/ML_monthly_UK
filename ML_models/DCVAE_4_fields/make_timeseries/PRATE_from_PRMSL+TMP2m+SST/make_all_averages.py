150c9b2c81ba543e1f36647dc62505c013413a49
Philip Brohan <philip@brohan.org>
2022-07-08 16:23:52 +0100

    Added scripts for TMP2m-constrained plots.

---
+++ ML_models/DCVAE_4_fields/make_timeseries/PRATE_from_PRMSL+TMP2m+SST/make_all_averages.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# Make 160 years of UK averages from all sources
+
+import os
+
+epoch = 990
+
+
+def is_done(year, month):
+    fn = "%s/ML_monthly_UK/UK_averages/PRMSL_TMP2m_SST/%04d/%04d/%02d.pkl" % (
+        os.getenv("SCRATCH"),
+        epoch,
+        year,
+        month,
+    )
+    if os.path.exists(fn):
+        return True
+    return False
+
+
+for year in range(1850, 1884):
+    for month in range(1, 13):
+        if is_done(year, month):
+            continue
+        cmd = (
+            "./make_averages_for_month.py --year=%04d " + "--month=%d --epoch=%d"
+        ) % (year, month, epoch,)
+        print(cmd)
