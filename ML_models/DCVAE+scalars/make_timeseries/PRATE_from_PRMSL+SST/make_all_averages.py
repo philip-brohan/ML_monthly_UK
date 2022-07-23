9bd0e3053b46d4e10d0866af2f73951a5c774122
Philip Brohan <philip@brohan.org>
2022-07-23 21:19:50 +0100

    Added validation and fit scripts for the model with imposed CO2 level.

---
+++ ML_models/DCVAE+scalars/make_timeseries/PRATE_from_PRMSL+SST/make_all_averages.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Make 160 years of UK averages from all sources
+
+import os
+
+epoch = 770
+
+
+def is_done(year, month):
+    fn = (
+        "%s/ML_monthly_UK/DCVAE+sclars/UK_averages/PRMSL_SST/"
+        + "%04d/%04d/%02d.pkl"
+    ) % (os.getenv("SCRATCH"), epoch, year, month,)
+    if os.path.exists(fn):
+        return True
+    return False
+
+
+for year in range(1884, 2015):
+    for month in range(1, 13):
+        if is_done(year, month):
+            continue
+        cmd = (
+            "./make_averages_for_month.py --year=%04d " + "--month=%d --epoch=%d"
+        ) % (year, month, epoch,)
+        print(cmd)
---
+++ ML_models/DCVAE+scalars/make_timeseries/PRATE_from_PRMSL+SST/make_all_fitted.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# Make 100+ years of fitted fields
+
+import os
+
+epoch = 770
+
+
+def is_done(year, month, member):
+    fn = (
+        "%s/ML_monthly_UK/DCVAE+scalars/fitted/constraints_PRMSL_SST/"
+        + "%04d/%04d/%02d/%02d.nc"
+    ) % (os.getenv("SCRATCH"), epoch, year, month, member,)
+    if os.path.exists(fn):
+        return True
+    return False
+
+
+for year in range(1884, 2015):
+    for month in range(1, 13):
+        for member in [1, 12, 24, 36, 48, 60, 72]:
+            if is_done(year, month, member):
+                continue
+            cmd = (
+                "../../fit_to_fields/fit_and_save.py --year=%04d "
+                + "--month=%d --member=%d --PRMSL --SST --epoch=%d"
+            ) % (year, month, member, epoch,)
+            print(cmd)
