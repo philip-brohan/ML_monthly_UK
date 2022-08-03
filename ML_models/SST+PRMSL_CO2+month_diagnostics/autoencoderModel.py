6d459c07cb757c33fe97981c1f18d541d7f1c017
Philip Brohan <philip@brohan.org>
2022-08-03 16:53:58 +0100

    Added a model including CO2 and month as diagnostics.

---
+++ ML_models/SST+PRMSL_CO2+month_diagnostics/autoencoderModel.py
@@ -0,0 +1,253 @@
+# Specify a Deep Convolutional Variational AutoEncoder
+#  for the HadUK-Grid monthly fields - with CO2 and month as scalar diagnostics
+
+import os
+import sys
+import tensorflow as tf
+import numpy as np
+
+
+# Hyperparameters
+# Ratio of RMSE to KLD in error
+RMSE_scale = 10000
+# Relative importances of each variable in error
+PRMSL_scale = 1.0
+SST_scale = 1.0
+T2M_scale = 1.0
+PRATE_scale = 1.0
+CO2_scale = 1.0
+MONTH_scale = 1.0
+
+
+class DCVAE(tf.keras.Model):
+    def __init__(self):
+        super(DCVAE, self).__init__()
+        self.latent_dim = 20
+        self.encoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(1440, 896, 4)),
+                tf.keras.layers.Conv2D(
+                    filters=5 * 4,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2D(
+                    filters=5 * 2,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2D(
+                    filters=10,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2D(
+                    filters=20,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2D(
+                    filters=40,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="relu",
+                ),
+                tf.keras.layers.Flatten(),
+                # No activation
+                tf.keras.layers.Dense(self.latent_dim + self.latent_dim),
+            ]
+        )
+
+        self.decoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(self.latent_dim,)),
+                tf.keras.layers.Dense(units=45 * 28 * 40, activation=tf.nn.relu),
+                tf.keras.layers.Reshape(target_shape=(45, 28, 40)),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=20,
+                    kernel_size=3,
+                    strides=2,
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=10,
+                    kernel_size=3,
+                    strides=2,
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=5 * 2,
+                    kernel_size=3,
+                    strides=2,
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=5 * 4,
+                    kernel_size=3,
+                    strides=2,
+                    padding="same",
+                    activation="elu",
+                ),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=4, kernel_size=3, strides=2, padding="same"
+                ),
+            ]
+        )
+
+        self.diagnose_co2 = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(self.latent_dim,)),
+                tf.keras.layers.Dense(units=self.latent_dim, activation=tf.nn.relu),
+                tf.keras.layers.Dense(units=1, activation=tf.nn.relu),
+            ]
+        )
+
+        self.diagnose_month = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(self.latent_dim,)),
+                tf.keras.layers.Dense(units=self.latent_dim, activation=tf.nn.relu),
+                tf.keras.layers.Dense(units=1, activation=tf.nn.relu),
+            ]
+        )
+
+    def encode(self, x):
+        field = x[0]
+        mean, logvar = tf.split(self.encoder(field), num_or_size_splits=2, axis=1)
+        return mean, logvar
+
+    def decode(self, z):
+        decoded = self.decoder(z)
+        c2 = self.diagnose_co2(z)
+        mn = self.diagnose_month(z)
+        return (decoded, c2, mn)
+
+    def call(self, x):
+        mean, logvar = self.encode(x)
+        latent = self.reparameterize(mean, logvar)
+        encoded = self.decode(latent)
+        return encoded
+
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * 0.5) + mean
+
+    # Decode each member of the batch several times, to make a sample
+    #  returns a 4d tensor (size, batch, y, x)
+    def sample_decode(self, mean, logvar, size=100):
+        encoded = []
+        eps = tf.random.normal(shape=(size, self.latent_dim))
+        mean = tf.unstack(mean, axis=0)
+        logvar = tf.unstack(logvar, axis=0)
+        for batchI in range(len(mean)):
+            latent = eps * tf.exp(logvar[batchI] * 0.5) + mean[batchI]
+            encoded.append(self.decode(latent))
+        return tf.stack(encoded, axis=0)
+
+    # Autoencode each member of the batch several times, to make a sample
+    def sample_call(self, x, size=100):
+        mean, logvar = self.encode(x)
+        encoded = self.sample_decode(mean, logvar, size=size)
+        return encoded
+
+
+def log_normal_pdf(sample, mean, logvar, raxis=1):
+    log2pi = tf.math.log(2.0 * np.pi)
+    return tf.reduce_sum(
+        -0.5 * ((sample - mean) ** 2.0 * tf.exp(-logvar) + logvar + log2pi), axis=raxis,
+    )
+
+
+def compute_loss(model, x):
+    mean, logvar = model.encode(x)
+    latent = model.reparameterize(mean, logvar)
+    encoded = model.decode(latent)
+    rmse_PRMSL = (
+        tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(
+                encoded[0][:, :, :, 0], x[0][:, :, :, 0]
+            ),
+            axis=[1],
+        )
+        * RMSE_scale
+        * PRMSL_scale
+    )
+    rmse_SST = (
+        tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(
+                encoded[0][:, :, :, 1], x[0][:, :, :, 1]
+            ),
+            axis=[1],
+        )
+        * RMSE_scale
+        * SST_scale
+    )
+    rmse_T2M = (
+        tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(
+                encoded[0][:, :, :, 2], x[0][:, :, :, 2]
+            ),
+            axis=[1],
+        )
+        * RMSE_scale
+        * T2M_scale
+    )
+    rmse_PRATE = (
+        tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(
+                encoded[0][:, :, :, 3], x[0][:, :, :, 3]
+            ),
+            axis=[1],
+        )
+        * RMSE_scale
+        * PRATE_scale
+    )
+    logpz = log_normal_pdf(latent, 0.0, 0.0) * -1
+    logqz_x = log_normal_pdf(latent, mean, logvar)
+    rmse_CO2 = (
+        tf.reduce_mean(tf.keras.metrics.mean_squared_error(encoded[1], x[1]), axis=[1],)
+        * RMSE_scale
+        * CO2_scale
+    )
+    rmse_MONTH = (
+        tf.reduce_mean(tf.keras.metrics.mean_squared_error(encoded[2], x[1]), axis=[1],)
+        * RMSE_scale
+        * MONTH_scale
+    )
+    return tf.stack(
+        [
+            rmse_PRMSL,
+            rmse_SST,
+            rmse_T2M,
+            rmse_PRATE,
+            logpz,
+            logqz_x,
+            rmse_CO2,
+            rmse_MONTH,
+        ]
+    )
+
+
+@tf.function  # Optimiser ~25% speedup on VDI (CPU-only)
+def train_step(model, x, optimizer):
+    """Executes one training step and returns the loss.
+
+    This function computes the loss and gradients, and uses the latter to
+    update the model's parameters.
+    """
+    with tf.GradientTape() as tape:
+        vstack = compute_loss(model, x)
+        metric = tf.reduce_mean(tf.math.reduce_sum(vstack, axis=0))
+    gradients = tape.gradient(metric, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
---
+++ ML_models/SST+PRMSL_CO2+month_diagnostics/fit_to_fields/fit_and_save.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+# Find a point in latent space that maximises the fit to some given input fields,
+#  and save the fitted state.
+
+
+import os
+import sys
+import numpy as np
+import iris
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+import warnings
+
+warnings.filterwarnings("ignore", message=".*TransverseMercator.*")
+
+# Going to do external parallelism - run this on one core
+tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(2)
+import dask
+
+dask.config.set(scheduler="single-threaded")
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epoch", help="Epoch", type=int, required=False, default=990)
+parser.add_argument(
+    "--year", help="Year to fit to", type=int, required=True,
+)
+parser.add_argument(
+    "--month", help="Month to fit to", type=int, required=True,
+)
+parser.add_argument(
+    "--member", help="Member to fit to", type=int, required=False, default=1
+)
+parser.add_argument(
+    "--PRMSL", help="Fit to PRMSL?", dest="PRMSL", default=False, action="store_true"
+)
+parser.add_argument(
+    "--SST", help="Fit to SST?", dest="SST", default=False, action="store_true"
+)
+parser.add_argument(
+    "--TMP2m", help="Fit to TMP2m?", dest="TMP2m", default=False, action="store_true"
+)
+parser.add_argument(
+    "--PRATE", help="Fit to PRATE?", dest="PRATE", default=False, action="store_true"
+)
+parser.add_argument(
+    "--iter", help="No. of iterations", type=int, required=False, default=1000,
+)
+args = parser.parse_args()
+
+sys.path.append("%s/../../../get_data" % os.path.dirname(__file__))
+from HUKG_monthly_load import load_cList
+from HUKG_monthly_load import sCube
+from HUKG_monthly_load import lm_20CR
+from HUKG_monthly_load import dm_hukg
+from TWCR_monthly_load import get_range
+
+sys.path.append("%s/../make_tensors" % os.path.dirname(__file__))
+from tensor_utils import cList_to_tensor
+from tensor_utils import tensor_to_cList
+from tensor_utils import normalise
+from tensor_utils import unnormalise
+
+
+# Load and standardise data
+qd = load_cList(args.year, args.month, args.member)
+ict = cList_to_tensor(qd, lm_20CR.data.mask, dm_hukg.data.mask)
+
+
+# Load the model specification
+sys.path.append("%s/.." % os.path.dirname(__file__))
+from localise import LSCRATCH
+from autoencoderModel import DCVAE
+from makeDataset import normalise_co2
+from makeDataset import normalise_month
+
+autoencoder = DCVAE()
+weights_dir = ("%s/models/Epoch_%04d") % (LSCRATCH, args.epoch,)
+load_status = autoencoder.load_weights("%s/ckpt" % weights_dir)
+# Check the load worked
+load_status.assert_existing_objects_matched()
+
+# We are using the model in inference mode - (does this have any effect?)
+autoencoder.decoder.trainable = False
+for layer in autoencoder.decoder.layers:
+    layer.trainable = False
+autoencoder.decoder.compile()
+
+latent = tf.Variable(tf.random.normal(shape=(1, autoencoder.latent_dim)))
+target = tf.constant(tf.reshape(ict, [1, 1440, 896, 4]))
+co2t = tf.reshape(
+    tf.convert_to_tensor(normalise_co2("%04d" % args.year), np.float32), [1, 1]
+)
+cmt = tf.reshape(
+    tf.convert_to_tensor(normalise_month("0000-%02d" % args.month), np.float32), [1, 1]
+)
+
+
+def decodeFit():
+    result = 0.0
+    decoded = autoencoder.decode(tf.concat([latent, co2t, cmt], axis=1))
+    if args.PRMSL:
+        result = result + tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(decoded[:, :, :, 0], target[:, :, :, 0])
+        )
+    if args.SST:
+        result = result + tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(
+                tf.boolean_mask(
+                    decoded[:, :, :, 1], np.invert(lm_20CR.data.mask), axis=1
+                ),
+                tf.boolean_mask(
+                    target[:, :, :, 1], np.invert(lm_20CR.data.mask), axis=1
+                ),
+            )
+        )
+    if args.TMP2m:
+        result = result + tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(
+                tf.boolean_mask(
+                    decoded[:, :, :, 2], np.invert(dm_hukg.data.mask), axis=1
+                ),
+                tf.boolean_mask(
+                    target[:, :, :, 2], np.invert(dm_hukg.data.mask), axis=1
+                ),
+            )
+        )
+    if args.PRATE:
+        result = result + tf.reduce_mean(
+            tf.keras.metrics.mean_squared_error(
+                tf.boolean_mask(
+                    decoded[:, :, :, 3], np.invert(dm_hukg.data.mask), axis=1
+                ),
+                tf.boolean_mask(
+                    target[:, :, :, 3], np.invert(dm_hukg.data.mask), axis=1
+                ),
+            )
+        )
+    return result
+
+
+if args.PRMSL or args.SST or args.TMP2m or args.PRATE:
+    loss = tfp.math.minimize(
+        decodeFit,
+        trainable_variables=[latent],
+        num_steps=args.iter,
+        optimizer=tf.optimizers.Adam(learning_rate=0.1),
+    )
+
+encoded = autoencoder.decode(tf.concat([latent, co2t, cmt], axis=1))
+
+fitted = tensor_to_cList(
+    encoded[0, :, :, :], sCube, lm_20CR.data.mask, dm_hukg.data.mask
+)
+
+opfile = "%s/fitted/constraints" % LSCRATCH
+for constraint in ["PRMSL", "PRATE", "TMP2m", "SST"]:
+    if vars(args)[constraint]:
+        opfile += "_%s" % constraint
+opfile += "/%04d/%04d/%02d/%02d.nc" % (args.epoch, args.year, args.month, args.member)
+
+if not os.path.isdir(os.path.dirname(opfile)):
+    os.makedirs(os.path.dirname(opfile))
+
+iris.save(fitted, opfile)
