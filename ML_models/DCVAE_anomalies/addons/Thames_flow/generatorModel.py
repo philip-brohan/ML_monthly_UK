5ab775c221d7b55753134cf3bfa1b1f95d81f826
Philip Brohan <philip@brohan.org>
2022-11-09 16:27:36 +0000

    Added model spec for the Thames flow calcuulation

---
+++ ML_models/DCVAE_anomalies/addons/Thames_flow/generatorModel.py
@@ -0,0 +1,73 @@
+# Model to generate Thames flow from latent space vector
+
+import tensorflow as tf
+
+
+class GeneratorM(tf.keras.Model):
+
+    # Initialiser - set up instance and define the models
+    def __init__(self):
+        super(GeneratorM, self).__init__()
+
+        # Hyperparameters
+        # Latent space dimension
+        self.latent_dim = 100
+        # Max gradient to apply in optimizer
+        self.max_gradient = 2.0
+
+        # Model to generate flow from latent space
+        self.generator = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(self.latent_dim,)),
+                tf.keras.layers.Dense(
+                    units=100,
+                    activation="elu",
+                    kernel_regularizer=tf.keras.regularizers.L2(0.01),
+                    activity_regularizer=tf.keras.regularizers.L2(0.1),
+                ),
+                tf.keras.layers.Dense(
+                    units=100,
+                    activation="elu",
+                    kernel_regularizer=tf.keras.regularizers.L2(0.01),
+                    activity_regularizer=tf.keras.regularizers.L2(0.1),
+                ),
+                tf.keras.layers.Dense(units=1, activation=None),
+            ]
+        )
+
+    # Call the generator model with a batch of points in latent space and return a
+    #  batch of flows
+    def call(self, z, training=True):
+        generated = self.generator(z, training=training)
+        return generated
+
+    @tf.function
+    def fit_loss(self, generated, target, climatology):
+
+        # Metric is fractional variance reduction compared to climatology
+        skill = tf.reduce_mean(tf.math.squared_difference(generated, target))
+        guess = tf.reduce_mean(tf.math.squared_difference(climatology, target))
+        return skill / guess
+
+    # Calculate the losses from autoencoding a batch of inputs
+    # We are calculating a seperate loss for each variable, and for for the
+    #  two components of the latent space KLD regularizer. This is useful
+    #  for monitoring and debugging, but the weight update only depends
+    #  on a single value (their sum).
+    @tf.function
+    def compute_loss(self, x, training):
+        generated = self.generate(x[0], training=training)
+        clim = generated * 0.0 + tf.reduce_mean(x[1])  # Climatology
+        loss = self.fit_loss(generated, x[0], clim)
+        return loss
+
+    # Run the generator for one batch, calculate the errors, calculate the
+    #  gradients and update the layer weights.
+    @tf.function
+    def train_on_batch(self, x, optimizer):
+        with tf.GradientTape() as tape:
+            overall_loss = self.compute_loss(x, training=True)
+        gradients = tape.gradient(overall_loss, self.trainable_variables)
+        # Clip the gradients - helps against sudden numerical problems
+        gradients = [tf.clip_by_norm(g, self.max_gradient) for g in gradients]
+        optimizer.apply_gradients(zip(gradients, self.trainable_variables))
---
+++ ML_models/DCVAE_anomalies/addons/Thames_flow/makeDataset.py
@@ -0,0 +1,93 @@
+# Make tf.data.Datasets from HadUK-Grid monthly latents and Thames flow data
+
+import os
+import sys
+import random
+import tensorflow as tf
+import numpy as np
+
+sys.path.append("%s/../.." % os.path.dirname(__file__))
+from localise import TSOURCE
+
+# Load the flow data
+fdata = {}
+with open(
+    "%s/../../../../get_data/NRFA/normalised.txt" % os.path.dirname(__file__)
+) as f:
+    for line in f.readlines():
+        if len(line) < 10:
+            continue
+        year = line[:4]
+        month = line[5:7]
+        flow = float(line[8:])
+        if year not in fdata:
+            fdata[year] = {}
+        fdata[year][month] = flow
+
+
+# Load a latent-space tensor from a file
+def load_latent(file_name):
+    sict = tf.io.read_file(file_name)
+    imt = tf.io.parse_tensor(sict, np.float32)
+    imt = tf.reshape(imt, [100])
+    return imt
+
+
+# Get a flow anomaly for a month given a latent-space tensor file name
+def load_flow(file_name):
+    fname = os.path.basename(file_name)
+    year = fname[:4]
+    month = fname[5:7]
+    return fdata[year][month]
+
+
+# Get a list of filenames containing tensors
+def getFileNames(purpose, nImages=None, startyear=1885, endyear=2020):
+    inFiles = sorted(os.listdir("%s/latents/%s" % (TSOURCE, purpose)))
+    if startyear is not None:
+        inFiles = [fn for fn in inFiles if int(fn[:4]) >= startyear]
+    if endyear is not None:
+        inFiles = [fn for fn in inFiles if int(fn[:4]) <= endyear]
+    if nImages is not None:
+        if len(inFiles) >= nImages:
+            inFiles = inFiles[0:nImages]
+        else:
+            raise ValueError(
+                "Only %d images available, can't provide %d" % (len(inFiles), nImages)
+            )
+    return inFiles
+
+
+# Get a dataset
+def getDataset(
+    purpose, nImages=None, startyear=1885, endyear=2020, shuffle=True, cache=False
+):
+
+    # Get a list of filenames containing LS tensors
+    inFiles = getFileNames(
+        purpose, nImages=nImages, startyear=startyear, endyear=endyear
+    )
+    if shuffle:
+        random.shuffle(inFiles)
+
+    # Create TensorFlow Dataset object from the latent-space file names
+    tn_data = tf.data.Dataset.from_tensor_slices(tf.constant(inFiles))
+
+    # Convert from list of file names to Dataset of source file contents
+    fnFiles = ["%s/latents/%s/%s" % (TSOURCE, purpose, x) for x in inFiles]
+    tl_data = tf.data.Dataset.from_tensor_slices(tf.constant(fnFiles))
+    tl_data = tl_data.map(load_latent, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    # Create a matching dataset of flows
+    tf_data = tf.data.Dataset.from_tensor_slices([load_flow(x) for x in inFiles])
+
+    # Zip the data together with the filenames (so we can find the date and source of each
+    #   data tensor if we need it).
+    tz_data = tf.data.Dataset.zip((tl_data, tf_data, tn_data))
+
+    # Optimisation
+    if cache:
+        tz_data = tz_data.cache()  # Great, iff you have enough RAM for it
+    tz_data = tz_data.prefetch(tf.data.experimental.AUTOTUNE)
+
+    return tz_data
