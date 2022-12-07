af13ab30a514579b038679e0f7d7dc37549ff22b
Philip Brohan <philip@brohan.org>
2022-12-07 17:23:17 +0000

    Added retrieval and access to Rainfall Rescue station data.

--- get_data/HadUKGrid/copy_HadUKGrid_data.sh
+++ get_data/HadUKGrid/copy_HadUKGrid_data.sh
@@ -30,3 +30,8 @@ mkdir -p $SCRATCH/haduk-grid/monthly_meantemp_climatology
 cp -rn /data/users/haduk/uk_climate_data/supported/haduk-grid/v1.1.0.0/data/grid_archives/lta_archive_v1/grid/monthly_meantemp_climatology/1961-1990 $SCRATCH/haduk-grid/monthly_meantemp_climatology/
 mkdir -p $SCRATCH/haduk-grid/monthly_rainfall_climatology
 cp -rn /data/users/haduk/uk_climate_data/supported/haduk-grid/v1.1.0.0/data/grid_archives/lta_archive_v1/grid/monthly_rainfall_climatology/1961-1990 $SCRATCH/haduk-grid/monthly_rainfall_climatology/
+
+# station data files
+mkdir -p $SCRATCH/haduk-grid/station_data
+cp -rn /data/users/haduk/uk_climate_data/supported/haduk-grid/v1.1.0.0/resources/cedar/cedar_2022-01-25.csv $SCRATCH/haduk-grid/station_data/metadata.csv
+cp -rn /data/users/haduk/uk_climate_data/supported/haduk-grid/v1.1.0.0/data/input_data/monthly_rainfall_rainfall-rescue_v1.1.0 $SCRATCH/haduk-grid/station_data
